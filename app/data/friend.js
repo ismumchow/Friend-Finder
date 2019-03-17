4aa76d267943d2e43f1207ef65dc4e64fe2e6fb0
ischdry <ichowdhury4@fordham.edu>
2019-03-16 22:44:15 -0400

    last update

---
+++ app/data/friend.js
@@ -0,0 +1,53 @@
+var friendsArray = [
+    {
+        "name": "Adam",
+        "photo": "https://boygeniusreport.files.wordpress.com/2017/12/screen-shot-2017-12-08-at-11-57-10-am.png?w=782",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "Brett",
+        "photo": "https://boygeniusreport.files.wordpress.com/2017/12/screen-shot-2017-12-08-at-11-57-10-am.png?w=782",
+        "scores": [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        "name": "Jeremiah",
+        "photo": "https://boygeniusreport.files.wordpress.com/2017/12/screen-shot-2017-12-08-at-11-57-10-am.png?w=782",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    }
+   
+]
+
+module.exports = friendsArray;
